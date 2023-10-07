f4dfd6935a2c3cf164dffc6be45712c2d4f4a801
Daza04 <dazaroaj@gmail.com>
2023-10-07 13:59:29 -0500

    feat: Add CSS styles Register page

--- src/modules/auth/pages/Register.tsx
+++ src/modules/auth/pages/Register.tsx
@@ -1,4 +1,4 @@
-import "../styles/Login.css"
+import "../styles/Login.css";
 
 export default function Register() {
   return (
@@ -7,22 +7,26 @@ export default function Register() {
 
       <form action="#">
 
-      <div className="form-group">
+        <div className="form-group">
           <label htmlFor="name">Nombre Establecimiento:</label>
-          <input type="text" id="name" name="name" required />
+          <input type="text" id="name" name="name" required className="w-full md:w-1/2" />
+          {/* La clase "w-full" hará que el ancho sea del 100% en dispositivos pequeños y "md:w-1/2" en dispositivos medianos */}
         </div>
+
         <div className="form-group">
           <label htmlFor="username">Usuario:</label>
-          <input type="text" id="username" name="username" required />
+          <input type="text" id="username" name="username" required className="w-full md:w-1/2" />
         </div>
+
         <div className="form-group">
           <label htmlFor="password">Contraseña:</label>
-          <input type="password" id="password" name="password" required />
+          <input type="password" id="password" name="password" required className="w-full md:w-1/2" />
         </div>
 
-        <button type="submit">Registrarse</button>
+        <button type="submit" className="mt-4 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+          Registrarse
+        </button>
       </form>
-
     </div>
   );
 }
