ac6759dd5245590e2f92c1a4c52a58f6cbc6a640
Alejandro-C <everjamioy2001@outlook.es>
2023-10-20 20:02:22 -0500

    feat: Adjust sports space detail interface

--- src/modules/home/pages/Room.tsx
+++ src/modules/home/pages/Room.tsx
@@ -35,19 +35,24 @@ export default function Room() {
       {loading ? (
         <p>Loading...</p>
       ) : (
-        <div className="container">
-        <div className="room-info">
-          <h1>{room.title}</h1>
-          <img src="https://static.motelnowapp.com/co/images/motel/10084.jpg" alt="Habitación Estándar" />
-          <p><strong>Descripción:</strong> {room.description} </p>
-          <p><strong>Precio:</strong> {room.price} </p>
-        </div>
+        <div className="container mx-auto mt-1 p-6 bg-white shadow-lg rounded-lg">
+          <div className="flex flex-col md:flex-row gap-4">
+            <div className="room-info md:w-1/2">
+              <h1 className="text-2xl font-semibold mb-4">{room.title}</h1>
+              <img src={room.imageUrl} alt="Habitación Estándar" className="mb-4 rounded-lg" />
+              <p><strong>Descripción:</strong> {room.description}</p>
+              <p><strong>Precio:</strong> {room.price}</p>
+            </div>
 
-        <div className="room-description">
-          <h2>Más detalles sobre la habitación:</h2>
-          <p>Esta habitación está equipada con todo lo que necesitas para una estancia cómoda. Incluye comodidades básicas para que te sientas como en casa durante tu viaje.</p>
+            <div className="room-description md:w-1/2 mt-4 md:mt-0">
+              <h2 className="text-xl font-semibold mb-2">Más detalles sobre la cancha deportiva:</h2>
+              <p className="text-gray-700">
+                Esta cancha deportiva está equipada con todo lo que necesitas para una experiencia cómoda y emocionante.
+                Incluye comodidades esenciales para que disfrutes al máximo de tus actividades deportivas y recreativas.
+              </p>
+            </div>
+          </div>
         </div>
-    </div>
       )}
     </HomeLayout>
   );
