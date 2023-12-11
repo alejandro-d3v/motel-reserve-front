aa0f011f0b413849dfcc19a0df5875abcf3a9102
Alejandro-C <everjamioy2001@outlook.es>
2023-12-11 00:29:00 -0500

    feat: Add interfaces for successful and failed payments

--- src/modules/home/home.routing.ts
+++ src/modules/home/home.routing.ts
@@ -3,6 +3,9 @@ import { RouteObject } from "react-router-dom";
 import Home from "./pages/Home";
 import Service from "./pages/Service";
 
+import PaymentFailure from "./pages/PaymentFailure";
+import PaymentSuccess from "./pages/PaymentSuccess";
+
 export const homeRouting: RouteObject[] = [
   {
     path: '',
@@ -11,5 +14,14 @@ export const homeRouting: RouteObject[] = [
   {
     path: 'service/:serviceId',
     Component: Service,
+  },
+
+  {
+    path: 'service/payment-failure',
+    Component: PaymentFailure,
+  },
+  {
+    path: 'service/payment-success',
+    Component: PaymentSuccess,
   }
 ];
---
+++ src/modules/home/pages/PaymentSuccess.tsx
@@ -0,0 +1,37 @@
+// import { useParams } from "react-router-dom";
+import { useEffect, useState } from "react";
+
+import HomeLayout from "../layouts/HomeLayout";
+
+import AppLoading from "../../../shared/components/AppLoading";
+
+export default function PaymentSuccess () {
+  // const params = useParams();
+
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const getQuery = async () => {
+      try {
+        console.log('query', 'show query');
+      } catch (e) {
+        console.error('err', e);
+        setTimeout(() => setLoading(false), 300);
+      }
+
+      setTimeout(() => setLoading(false), 300);
+    };
+
+    getQuery();
+  }, []);
+
+  return (
+    <HomeLayout pageName='Pago fallido'>
+      {loading ? ( <AppLoading /> ) : (
+        <>
+          <h1>Pago fallido</h1>
+        </>
+      )}
+    </HomeLayout>
+  );
+}
