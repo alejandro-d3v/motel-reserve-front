79afd56aa603e323ea6293134c8b560a9f79e49e
Alejandro-C <everjamioy2001@outlook.es>
2023-12-17 12:54:11 -0500

    feat: update DashboardBase component

--- src/modules/adminHome/components/CardGridMap.tsx
+++ src/modules/adminHome/components/CardGridMap.tsx
@@ -1,56 +1,100 @@
-import { BadgeDelta, Card, Flex, Grid, Metric, ProgressBar, Text } from "@tremor/react"
+import { BadgeDelta, Card, Flex, Grid, Metric, Text } from "@tremor/react"
+import { useEffect, useState } from "react";
+
+import AppLoading from "../../../shared/components/AppLoading";
+
+import { ServiceWithReservationsDto } from "../../services/dtos/service.dto";
+
+import { GetServicesWithReservationsService } from "../services/getServicesWithReservations.service";
+
+const getServicesWithReservationsService = new GetServicesWithReservationsService();
 
 const CardGridMap = () => {
+  const [report, setReport] = useState<any[]>([
+    {
+      title: 'Número total de canchas registradas',
+      metric: '0',
+    },
+    {
+      title: 'Total de reservas realizadas',
+      metric: '0',
+      delta: '23.9%',
+    },
+    {
+      title: 'Total de ingresos generados',
+      metric: '0',
+    },
+  ]);
+  const [services, setServices] = useState<ServiceWithReservationsDto[] | []>([]);
 
-    const data = [
-        {
-            title: 'Ventas',
-            metric: '$ 12,699',
-            progress: 15.9,
-            target: '$ 80,000',
-            delta: '13.2%',
-            deltaType: 'moderateIncrease',
-        },
-        {
-            title: 'Ganancias',
-            metric: '$ 45,564',
-            progress: 36.5,
-            target: '$ 125,000',
-            delta: '23.9%',
-            deltaType: 'increase',
-        },
-        {
-            title: 'Clientes',
-            metric: '1,072',
-            progress: 53.6,
-            target: '2,000',
-            delta: '10.1%',
-            deltaType: 'moderateDecrease',
-        },
-    ]
-    
+  const [loading, setLoading] = useState(true);
 
-    return (
-      <Grid numItemsMd={2} numItemsLg={3} className="mt-6 gap-2"  >
-          { data.map( (item) => (
-              <Card key={item.title}>
-                   <Flex className='items-start'>
-                      <div>
-                          <Text>{item.title}</Text>
-                          <Metric>{item.metric}</Metric>
-                      </div>
-                      <BadgeDelta>{item.delta}</BadgeDelta>
-                   </Flex>   
-  
-                   <Flex className='mt-4 space-x-2'>
-                      <Text>{`${item.progress}% (${item.metric})`}</Text>
-                      <Text>{item.target}</Text>
-                   </Flex>
-                  <ProgressBar value={item.progress} className="mt-3"/> 
-  
-              </Card>    
-          )) }
-      </Grid>
-    )
-  }
-    export default CardGridMap
+  useEffect(() => {
+    const getData = async () => {
+      try {
+        const fetchedServices = await getServicesWithReservationsService.run({});
+        setServices(fetchedServices);
+      } catch (e) {
+        console.error('err:', e);
+        setTimeout(() => setLoading(false), 400);
+      }
+    };
+
+    getData();
+  }, []);
+
+  useEffect(() => {
+    const totalReservas = services.reduce((acc, item) => acc + item.reservation.length, 0);
+
+    const totalIngresos = services.reduce((acc, item) => {
+      return acc + item.reservation.reduce((total, reserva) => {
+        if (reserva.paymentStatus === 1 || reserva.paymentStatus === 3) {
+          return total + reserva.totalPrice;
+        }
+        return total;
+      }, 0);
+    }, 0);
+
+    const newReport = [
+      {
+        title: 'Número total de canchas registradas',
+        metric: services.length,
+      },
+      {
+        title: 'Total de reservas realizadas',
+        metric: totalReservas,
+      },
+      {
+        title: 'Total de ingresos generados',
+        metric: `$ ${Intl.NumberFormat().format(totalIngresos ?? 0)}`,
+      },
+    ];
+
+    setReport(newReport);
+
+    setTimeout(() => setLoading(false), 400);
+  }, [services]);
+
+  return (
+    <>
+      {loading ? ( <AppLoading /> ) : (
+        <Grid numItemsMd={2} numItemsLg={3} className="mt-6 gap-2"  >
+          { report.map( (item) => (
+            <Card key={item.title}>
+              <Flex className='items-start'>
+                <div>
+                  <Text>{item.title}</Text>
+                  <Metric>{item.metric}</Metric>
+                </div>
+
+                { item.delta && <BadgeDelta>{item.delta}</BadgeDelta> }
+              </Flex> 
+            </Card>    
+          ))}
+        </Grid>
+      )}
+    </>
+  )
+}
+
+export default CardGridMap
--- src/modules/adminHome/components/ChartDonut.tsx
+++ src/modules/adminHome/components/ChartDonut.tsx
@@ -1,46 +1,71 @@
 import { Card, DonutChart, Title } from "@tremor/react"
 
+import AppLoading from "../../../shared/components/AppLoading";
+
+import { ServiceWithReservationsDto } from "../../services/dtos/service.dto";
+
+import { GetServicesWithReservationsService } from "../services/getServicesWithReservations.service";
+import { useEffect, useState } from "react";
+
+const getServicesWithReservationsService = new GetServicesWithReservationsService();
+
 const ChartDonut = () => {
+  const [report, setReport] = useState<any[]>([]);
+  const [services, setServices] = useState<ServiceWithReservationsDto[] | []>([]);
 
-    const cities = [
-        {
-            name: 'Cancha #1',
-            sales: 9800
-        },
-        {
-            name: 'Cancha #2',
-            sales: 4567,
-        },
-        {
-            name: 'Cancha #3',
-            sales: 3908,
-        },
-        {
-            name: 'Cancha #4',
-            sales: 2400,
-        },
-        {
-            name: 'Cancha #5',
-            sales: 1908,
-        },
-        {
-            name: 'Cancha #6',
-            sales: 1398,
-        },
-    ]
+  const [loading, setLoading] = useState(true);
 
-    return (
-      <Card>
-          <Title>Ventas por canchas</Title>
-          <DonutChart 
-              data={cities}
+  useEffect(() => {
+    const getData = async () => {
+      try {
+        const fetchedServices = await getServicesWithReservationsService.run({});
+        setServices(fetchedServices);
+      } catch (e) {
+        console.error('err:', e);
+        setTimeout(() => setLoading(false), 400);
+      }
+    };
+
+    getData();
+  }, []);
+
+  useEffect(() => {
+    const newArray = services.map(service => {
+      const serviceName = service.name;
+      const totalSales = service.reservation
+        .filter(reservation => reservation.paymentStatus === 1 || reservation.paymentStatus === 3)
+        .reduce((acc, reservation) => acc + reservation.totalPrice, 0);
+    
+      return {
+        name: serviceName,
+        sales: totalSales,
+      };
+    });
+
+    setReport(newArray);
+
+    setTimeout(() => setLoading(false), 400);
+  }, [services]);
+
+  return (
+    <Card>
+      <Title>Ingresos por canchas</Title>
+
+      {loading ? ( <AppLoading /> ) : (
+        <>
+          { report.length && !loading && (
+            <DonutChart
+              data={report}
               category='sales'
               index='name'
               className='mt-6'
               colors={["yellow", "violet", "indigo", "rose", "cyan", "green"]}
-          />
-      </Card>
-    )
-  }
+            />
+          )}
+        </>
+      )}
+    </Card>
+  )
+}
   
-  export default ChartDonut
+export default ChartDonut
